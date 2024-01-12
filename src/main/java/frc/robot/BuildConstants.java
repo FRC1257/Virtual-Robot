2721623c38016a95af74930cbb05539171da28d0
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-01-12 13:53:16 -0500

    add yagsl

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Virtual-Robot-Imported";
+  public static final String MAVEN_NAME = "Virtual-Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2023-12-07 22:17:16 EST";
-  public static final long BUILD_UNIX_TIME = 1702005436731L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 19;
+  public static final String GIT_SHA = "c888722d10c94ef5a3c248c53bae052b66aa54ea";
+  public static final String GIT_DATE = "2023-12-07 22:24:11 EST";
+  public static final String GIT_BRANCH = "swerve-2024";
+  public static final String BUILD_DATE = "2024-01-05 21:20:53 EST";
+  public static final long BUILD_UNIX_TIME = 1704507653208L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
